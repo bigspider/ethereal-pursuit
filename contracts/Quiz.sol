d424ad54bebb3a5de881e1787423c504d019f2bf
Salvatore Ingala <salvatore.ingala@gmail.com>
2019-01-23 11:10:06 +0000

    First attempt at full contract

--- contracts/Quiz.sol
+++ contracts/Quiz.sol
@@ -1,84 +1,135 @@
 pragma solidity >=0.4.21 <0.6.0;
 
 contract Quiz {
-  address public owner;
+    address public owner;
 
-  uint8 phase = 0;
+    uint8 phase = 0;
 
-  string question;
-  bytes32 rightAnswerCommitment; // Commitment of the correct answer
-  bytes32 rightAnswer; // Actual answer, after reveal
-  
-  uint n_winners = 0;
-  mapping(address => bytes32) answers;  // Commitment of the answers for each user
-  mapping(address => bool) userWon;     // true if user won
+    uint32 quiz_number = 0;
+    string question;
+    bytes32 rightAnswerCommitment; // Commitment of the correct answer
+    bytes32 rightAnswer; // Actual answer, after reveal
+    
+    uint n_winners = 0; // Number of winners for this round
+    uint prize_amount; // Amount that winner can withdraw
+    
+    mapping(address => bytes32) answers;    // Commitment of the answers for each user
+    mapping(address => uint32) userWon;    // userWon[addr] is set to quiz_number if the user gave the right answer
 
-  constructor() public {
-    owner = msg.sender;
-  }
+    constructor() public {
+        owner = msg.sender;
+    }
 
-  modifier onlyOwner() {
-    if (msg.sender == owner) _;
-  }
+    modifier onlyOwner() {
+        if (msg.sender == owner) _;
+    }
 
-  modifier onlyInitPhase() {
-    if (phase == 0) _;
-  }
+    modifier onlyInitPhase() {
+        if (phase == 0) _;
+    }
 
-  modifier onlyCommitPhase() {
-    if (phase == 1) _;
-  }
+    modifier onlyCommitPhase() {
+        if (phase == 1) _;
+    }
 
-  modifier onlyClaimPhase() {
-    if (phase == 2) _;
-  }
+    modifier onlyClaimPhase() {
+        if (phase == 2) _;
+    }
 
-  modifier onlyWithdrawPhase() {
-    if (phase == 3) _;
-  }
+    modifier onlyWithdrawPhase() {
+        if (phase == 3) _;
+    }
 
-  
-  // 0: INIT - Please commit hashed answer
-  
-  function initQuiz(string memory _question, bytes32 _rightAnswer) onlyInitPhase onlyOwner public {
-      question = _question;
-      rightAnswerCommitment = _rightAnswer;   
-      phase += 1;
-  }
-  
-   // Returns current question
-  function whichQuestion() onlyOwner public returns (string memory ){
+
+    // This modifier requires a certain
+    // fee being associated with a function call.
+    // If the caller sent too much, he or she is
+    // refunded, but only after the function body.
+    // This was dangerous before Solidity version 0.4.0,
+    // where it was possible to skip the part after `_;`.
+    modifier costs(uint _amount) {
+        require(
+            msg.value >= _amount,
+            "Not enough Ether provided."
+        );
+        _;
+        if (msg.value > _amount)
+            msg.sender.send(msg.value - _amount);
+    }
+
+    // Returns current question
+    // TODO: Redundant? question is public
+    function whichQuestion() onlyOwner public returns (string memory ){
         return question;
-  }
-  
-  // Returns current phase
-  function whichPhase() onlyOwner public returns (uint8 ){
+    }
+    
+    // Returns current phase
+    // TODO: Redundant? phase is public
+    function whichPhase() onlyOwner public returns (uint8 ){
         return phase;
-  }
+    }
+    
+    // 0: INIT - Please commit hashed answer
+    
+    function initQuiz(string memory _question, bytes32 _rightAnswerCommitment) onlyInitPhase onlyOwner public {
+        question = _question;
+        rightAnswerCommitment = _rightAnswerCommitment;
+        phase += 1;
+    }
 
 
+    // 1: COMMIT
+    function commitAnswer(bytes32 user_answer) onlyCommitPhase public payable costs(1 ether) {
+        //TODO: check timeout for phase 1
+        
+        answers[msg.sender] = user_answer;
+    }
 
-  // 1: COMMIT
-  function commitAnswer(bytes32 answer) onlyCommitPhase public {
-      // TODO
-  }
+    function startClaimPhase() onlyCommitPhase onlyOwner public {
+        //TODO: require timeout expiry
+        
+        phase += 1;
+    }
 
-  // 2: CLAIM
-  function claimRightAnswer(bytes32 answer, bytes32 randomness) onlyClaimPhase public {
-    //TODO: check correctness, update n_winners and userWon
-  }
-  
-  function startWithdrawals() onlyClaimPhase onlyOwner public {
-      //TODO: move to phase 3
-  }
+    // 2: CLAIM
+    function claimRightAnswer(bytes32 user_answer, bytes32 randomness) onlyClaimPhase public {
+        //TODO: check timeout
+
+        require(
+            keccak256(abi.encodePacked(quiz_number, user_answer, randomness)) == answers[msg.sender],
+            "The answer is wrong or malformed."
+        );
+        
+        n_winners += 1;
+        userWon[msg.sender] = quiz_number;
+    }
+    
+    function startWithdrawals() onlyClaimPhase onlyOwner public {
+        //TODO: require timeout expiry
+        
+        prize_amount = address(this).balance / n_winners;
+        
+        phase += 1;
+    }
 
 
-  // 3: WITHDRAW
-  function withdrawPrize() onlyWithdrawPhase public {
-      //TODO: allow withdrawal of the prize if user won
-  }
-  
-  function cleanup() onlyWithdrawPhase onlyOwner public returns (uint8 ){
-      phase = 0;
-  }
+    // 3: WITHDRAW
+    function withdrawPrize() onlyWithdrawPhase public {
+        if (userWon[msg.sender] == quiz_number) {
+            //Make sure user can withdraw only once
+            delete userWon[msg.sender];
+            delete answers[msg.sender];
+            
+            msg.sender.transfer(prize_amount);
+        }
+    }
+    
+    function cleanup() onlyWithdrawPhase onlyOwner public {
+        //Transfer any remaining balance to the owner (== the caller)
+        msg.sender.transfer(address(this).balance);
+        
+        phase = 0;
+        n_winners = 0;
+        quiz_number += 1;
+    }
 }
